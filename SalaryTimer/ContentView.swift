a4c4ddde70e3d879d9d91a8dcfd00f7676293f26
Eric Feng <ericzheyu.feng@gmail.com>
2025-05-20 15:41:24 -0500

    Initial Commit

---
+++ SalaryTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SalaryTimer
+//
+//  Created by Eric Feng on 5/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SalaryTimer/SalaryTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalaryTimerApp.swift
+//  SalaryTimer
+//
+//  Created by Eric Feng on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SalaryTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
